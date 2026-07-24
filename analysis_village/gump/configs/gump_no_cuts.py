b008d51a59113e6f92f1519372958f02b5a0778a
adithya04rao <adithya04rao@gmail.com>
2026-07-24 13:29:08 -0500

    Update gump_no_cuts.py

--- analysis_village/gump/configs/gump_no_cuts.py
+++ analysis_village/gump/configs/gump_no_cuts.py
@@ -1,9 +1,10 @@
 from analysis_village.gump.makedf import *
+#from makedf.make_mnp_df import make_recodf
 
 DFS = [make_pandora_no_cuts_df, make_gump_nudf, make_stubs, 
     # make_crthitdf, 
-    make_hdrdf, make_triggerdf, make_potdf_bnb, make_opflashdf]
+    make_hdrdf, make_triggerdf, make_potdf_bnb, make_opflashdf, make_mcprimdf, make_corrections_df, make_recodf]
 
 NAMES = ["evt", "mcnu", "stub", 
     # "crt", 
-    "hdr", "trig", "bnb", "flash"]
+    "hdr", "trig", "bnb", "flash", "prim", "corE", "reco"]
